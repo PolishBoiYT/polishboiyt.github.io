c44d912b6623eef3fd7c1738e5fa1fa43460367e
nSwishBall <157294157+TOPBYT-GitHub@users.noreply.github.com>
2024-08-16 18:32:19 +0200

    add epic stuff

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
     $(".notyapping").hide();
+    $(".why").hide();
+    $(".why2").hide();
 });
 
 function stopyapping() {
@@ -7,7 +9,20 @@ function stopyapping() {
     $(".stopyapping").hide();
     $(".notyapping").show();
 }
+async function POLSKAA() {
+    $(".icon").hide();
+    $(".why").show();
+    await sleep(1500);
+    $(".why2").show();
+    $(".why").hide();
+    await sleep(1500);
+    $(".why2").hide();
+}
 
+function sleep(ms) {
+    // wait for a specified time
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
 function loadstuff() {
     var funstuffPage = "funstuff/index.html";
     window.location = funstuffPage;
