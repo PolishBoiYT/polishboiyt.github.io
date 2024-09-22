942018231956543c835b4773f966dee5c90168bd
nSwishBall <157294157+TOPBYT-GitHub@users.noreply.github.com>
2024-09-22 10:57:36 +0200

    Update the website

--- main.js
+++ main.js
@@ -1,29 +1,38 @@
 $(document).ready(function() {
-    $(".notyapping").hide();
-    $(".why").hide();
-    $(".why2").hide();
+    $.easing.easeInOutQuad = function(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+    };
+
+    const clickSound = $('#click-sound')[0];
+    const gameSound = $('#game-sound')[0];
+
+    $('.nav-links a, .play-audio').on('click', function(event) {
+        event.preventDefault();
+        clickSound.play();
+
+        if ($(this).hasClass('play-audio')) {
+            gameSound.play();
+        }
+
+        const target = $(this).attr('href');
+        
+        if (target && target !== '#') {
+            const $targetSection = $(target);
+            if ($targetSection.length) {
+                $('section').removeClass('visible').css('opacity', 0);
+                $targetSection.addClass('visible').css('opacity', 1);
+
+                $('html, body').animate({
+                    scrollTop: $targetSection.offset().top
+                }, 800, 'easeInOutQuad');
+            }
+        }
+    });
 });
 
-function stopyapping() {
-    $(".yapping").hide();
-    $(".stopyapping").hide();
-    $(".notyapping").show();
-}
-async function POLSKAA() {
-    $(".icon").hide();
-    $(".why").show();
-    await sleep(1500);
-    $(".why2").show();
-    $(".why").hide();
-    await sleep(1500);
-    $(".why2").hide();
-}
-
-function sleep(ms) {
-    // wait for a specified time
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
-function loadstuff() {
-    var funstuffPage = "funstuff/index.html";
-    window.location = funstuffPage;
+function foundme() {
+	alert("You've found the hidden easter egg!")
 }
